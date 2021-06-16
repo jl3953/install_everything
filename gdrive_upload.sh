0b08a42629ccbb6389525862abf0d4455de45636
Jennifer Lam <jl87@princeton.edu>
2021-06-16 14:39:21 -0600

    add gdrive_upload.sh

---
+++ gdrive_upload.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/bash
+
+gdrive upload --recursive --parent 1NB3bnz_4icqqi4osx7mG0dcwjrMfQQym $1
--- install_gdrive.py
+++ install_gdrive.py
@@ -1,20 +1,19 @@
+import subprocess
 import sys
+
+
 import utils
 
 
 def install_gdrive():
-    utils.call("wget https://docs.google.com/uc?id=0B3X9GlR6EmbnWksyTEtCM0VfaFE&export=download /root/gdrive")
+    utils.call("wget --output-document=/root/gdrive https://docs.google.com/uc?id=0B3X9GlR6EmbnWksyTEtCM0VfaFE&export=download")
     utils.call("chmod +x /root/gdrive")
     utils.call("install /root/gdrive /usr/local/bin/gdrive")
-
-
-def authenticate_gdrive():
-    utils.call("gdrive list")
+    utils.call("install /root/install_everything/gdrive_upload.sh /usr/local/bin/gdrive_upload")
 
 
 def main():
     install_gdrive()
-    authenticate_gdrive()
 
     return 0
 
