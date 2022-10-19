91eb4295fdcfa4d36b55d9ee2330849ba759bbd7
Jennifer Lam <jl87@princeton.edu>
2022-10-19 14:42:00 -0400

    add '-y' to apt install

--- main.py
+++ main.py
@@ -136,10 +136,10 @@ def install_grpc_go():
 def install_smdbrpc_dependencies():
     # YOU BETTER HAVE INSTALLED GO AT THIS POINT
     utils.call("apt install -y protobuf-compiler")
-    utils.call("apt install python3-pip")
+    utils.call("apt install -y python3-pip")
     utils.call("python3 -m pip install grpcio")
     utils.call("python3 -m pip install grpcio-tools")
-    utils.call("apt-get install libpq-dev")
+    utils.call("apt-get install -y libpq-dev")
     utils.call("python3 -m pip install psycopg2")
 
 
